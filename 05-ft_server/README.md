50229ebfc872e7398b80275bf55f7449f763dd76
Michele Sessa <msessa@c1r18s3.42lisboa.com>
2021-05-03 17:53:53 +0100

    ft_server finished

---
+++ 05-ft_server/README.md
@@ -0,0 +1,15 @@
+# ft_server (42 Lisbon)
+
+## Setup
+- To build: `docker build -t ft_server:msessa .`
+- To run: `docker run -it -p 80:80 -p 443:443 ft_server:msessa`
+
+## Useful Resources
+
+[Setup Docker on 42 Macs](https://github.com/alexandregv/42toolbox)
+
+[Access websites with unsafe SSL certificate](https://dblazeski.medium.com/chrome-bypass-net-err-cert-invalid-for-development-daefae43eb12)
+
+[Full guide for ft_server - part 1](https://forhjy.medium.com/how-to-install-lemp-wordpress-on-debian-buster-by-using-dockerfile-1-75ddf3ede861)
+
+[Full guide for ft_server - part 2](https://forhjy.medium.com/42-ft-server-how-to-install-lemp-wordpress-on-debian-buster-by-using-dockerfile-2-4042adb2ab2c)
--- 05-ft_server/ft_server/srcs/start_nginx.sh
+++ 05-ft_server/ft_server/srcs/start_nginx.sh
@@ -1,17 +1,31 @@
 # **************************************************************************** #
 #                                                                              #
 #                                                         :::      ::::::::    #
-#    start_container.sh                                 :+:      :+:    :+:    #
+#    start_nginx.sh                                     :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
 #    By: msessa <marvin@42.fr>                      +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/04/30 16:34:16 by msessa            #+#    #+#              #
-#    Updated: 2021/05/03 15:13:39 by msessa           ###   ########.fr        #
+#    Updated: 2021/05/03 16:16:43 by msessa           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-bash start_nginx.sh
-service mysql start
-service php7.3-fpm start
+rm -f /etc/nginx/sites-enabled/default
 
-bash
+if [ $# == 1 ]
+	then
+	if [ $1 == 'on' ]
+		then
+			export autoindex='on'
+		elif [ $1 == 'off' ]
+			then
+				export autoindex='off'
+	fi
+fi
+if [ $autoindex == 'on' ]
+	then
+		ln -s /etc/nginx/sites-available/default /etc/nginx/sites-enabled/
+	else
+		ln -s /etc/nginx/sites-available/no-autoindex /etc/nginx/sites-enabled/default
+fi
+service nginx restart
