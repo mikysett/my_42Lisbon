d448dbc19184788e3433b46dc1640e9963819ef3
mikysett <mikysett@gmail.com>
2021-05-03 09:10:00 +0100

    ft_server working on

--- 05-ft_server/ft_server/srcs/start_container.sh
+++ 05-ft_server/ft_server/srcs/start_container.sh
@@ -6,8 +6,15 @@
 #    By: msessa <marvin@42.fr>                      +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/04/30 16:34:16 by msessa            #+#    #+#              #
-#    Updated: 2021/04/30 18:06:42 by msessa           ###   ########.fr        #
+#    Updated: 2021/05/01 12:13:11 by msessa           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
+
+# service mysql start
+# echo "CREATE DATABASE wpdb;" | mysql -u root
+# echo "CREATE USER 'wpuser'@'localhost' identified by 'dbpassword';" | mysql -u root
+# echo "GRANT ALL PRIVILEGES ON wpdb.* TO 'wpuser'@'localhost';" | mysql -u root
+# echo "FLUSH PRIVILEGES;" | mysql -u root
+# echo "EXIT" | mysql -u root
 
 bash
